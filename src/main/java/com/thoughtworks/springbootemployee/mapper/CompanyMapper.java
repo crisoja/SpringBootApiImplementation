3945c6a71c38acb02c668692b04601c96df6e94d
Janley Crsiologo <crisoja@oocl.com>
2021-08-14 18:37:47 +0800

    [Mapper&ResponseHomeworkEmployeAndComapany][Janley] refactor: add @Component annotation

--- src/main/java/com/thoughtworks/springbootemployee/mapper/CompanyMapper.java
+++ src/main/java/com/thoughtworks/springbootemployee/mapper/CompanyMapper.java
@@ -3,8 +3,12 @@ package com.thoughtworks.springbootemployee.mapper;
 import com.thoughtworks.springbootemployee.model.Company;
 import com.thoughtworks.springbootemployee.dto.CompanyResponse;
 import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Component;
 
+@Component
 public class CompanyMapper {
+
+
     public CompanyResponse toResponse(Company company){
         CompanyResponse companyResponse = new CompanyResponse();
         BeanUtils.copyProperties(company, companyResponse);

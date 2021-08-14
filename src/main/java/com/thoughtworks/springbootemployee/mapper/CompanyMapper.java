57880c60dc0f617b99cbb8312dee40fadd912f0b
Janley Crsiologo <crisoja@oocl.com>
2021-08-14 18:38:46 +0800

    [Mapper&ResponseHomeworkEmployeAndComapany][Janley] feat: created method toEntity with Employee return type

--- src/main/java/com/thoughtworks/springbootemployee/mapper/CompanyMapper.java
+++ src/main/java/com/thoughtworks/springbootemployee/mapper/CompanyMapper.java
@@ -1,18 +1,28 @@
 package com.thoughtworks.springbootemployee.mapper;
 
+import com.thoughtworks.springbootemployee.dto.EmployeeRequest;
 import com.thoughtworks.springbootemployee.model.Company;
 import com.thoughtworks.springbootemployee.dto.CompanyResponse;
+import com.thoughtworks.springbootemployee.model.Employee;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CompanyMapper {
 
-
     public CompanyResponse toResponse(Company company){
         CompanyResponse companyResponse = new CompanyResponse();
         BeanUtils.copyProperties(company, companyResponse);
         companyResponse.setEmployeeNumber(company.getEmployees().size());
+
         return companyResponse;
     }
+
+    public Employee toEntity(EmployeeRequest employeeRequest){
+        Employee employee = new Employee();
+        BeanUtils.copyProperties(employeeRequest, employee);
+
+        return employee;
+    }
+
 }

5437a2a95d046bb66a6cadad852fafc6368dc5d7
Janley Crsiologo <crisoja@oocl.com>
2021-08-14 15:04:17 +0800

    [CompanyIntegrationTest][Janley] test: should_return_all_companies_when_find_api

---
+++ src/test/java/com/thoughtworks/springbootemployee/integration/CompanyIntegrationTest.java
@@ -0,0 +1,39 @@
+package com.thoughtworks.springbootemployee.integration;
+
+import com.thoughtworks.springbootemployee.model.Company;
+import com.thoughtworks.springbootemployee.repository.CompanyRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class CompanyIntegrationTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    @AfterEach
+    public void tearDown(){
+        companyRepository.deleteAll();
+    }
+
+    @Test
+    void should_return_all_companies_when_find_api() throws Exception {
+        final Company company = new Company("OOCL");
+        companyRepository.save(company);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/companies"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id").isNumber())
+                .andExpect(jsonPath("$[0].companyName").value("OOCL"));
+    }
+}

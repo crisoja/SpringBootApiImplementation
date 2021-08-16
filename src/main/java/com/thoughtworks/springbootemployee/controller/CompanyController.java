6f3456415dcf3c26fd71427b6b947fc13bf871de
Janley Crsiologo <crisoja@oocl.com>
2021-08-16 14:10:29 +0800

    [Mapper&ResponseHomeworkEmployeAndComapany][Janley] refactor: rename findEmployeesByPagination to findCompaniesByPagination

--- src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
@@ -64,7 +64,7 @@ public class CompanyController {
     }
 
     @GetMapping(params = {"pageIndex", "pageSize"})
-    public List<CompanyResponse> findEmployeesByPagination(@RequestParam Integer pageIndex, @RequestParam Integer pageSize){
+    public List<CompanyResponse> findCompaniesByPagination(@RequestParam Integer pageIndex, @RequestParam Integer pageSize){
         return companyMapper.toResponse(companyService.findCompaniesByPagination(pageIndex, pageSize));
     }
 
